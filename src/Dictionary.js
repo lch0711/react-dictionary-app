23ea5558d7b0ecd149f1677571e06bf54e75b430
lch0711 <lchung0711@gmail.com>
2021-11-21 19:18:33 +0000

    Added definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import Results from "./Results";
+
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function Search(event) {
@@ -23,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={Search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
